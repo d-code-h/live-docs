854a7704b35aeb041819faac385989964f928cfe
Habeeb Yunus Habeeb <habeebdh1@outlook.com>
2024-11-26 02:37:12 +0100

    Fix type deployment issues

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 declare type SearchParamProps = {
-  params: { [key: string]: string };
-  searchParams: { [key: string]: string | string[] | undefined };
+  params: Promise<{ id: string }>;
+  // params: { [key: string]: string };
+  // searchParams: { [key: string]: string | string[] | undefined };
 };
 
 declare type AccessType = ['room:write'] | ['room:read', 'room:presence:write'];
